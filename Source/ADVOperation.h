f52a45db951fc8c0fc631b65c4ed66d306f965e1
Guillaume Sempe <guillaume.sempe@gmail.com>
2015-06-15 22:27:44 +0200

    Create project and add Apple files
    Add Operation classes from the Apple sample code Advanced-NSOperations.

---
+++ Source/ADVOperation.h
@@ -0,0 +1,19 @@
+//
+//  ADVOperation.h
+//  ADVOperation
+//
+//  Created by Guillaume Sempé on 15/06/15.
+//  Copyright © 2015 Advanced Operation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ADVOperation.
+FOUNDATION_EXPORT double ADVOperationVersionNumber;
+
+//! Project version string for ADVOperation.
+FOUNDATION_EXPORT const unsigned char ADVOperationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ADVOperation/PublicHeader.h>
+
+
